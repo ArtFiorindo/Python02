84fd135b3b88f0269e446c42072fdb41ad651e9d
ArtFiorindo <alopesfiorindo@gmail.com>
2023-09-21 22:34:46 -0300

    Correção 21-09 1.6

--- python_Sistema_de_Repetição/Aula08Ex29.py
+++ python_Sistema_de_Repetição/Aula08Ex29.py
@@ -1,10 +1,9 @@
-for num in range(1, 21):
+for num in range(1, 21, 1):
 
-    input("pressione enter")
-    for i in range(1, 11):
+    
+    for i in range(1, 11, 1):
         t = num * i
         print(f"{num} x {i} = {t}")
 
-
-input("pressione enter")
+    input("pressione enter")
 
