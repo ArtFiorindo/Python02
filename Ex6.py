45a2fd2c81355050feb5dcdbab28e3fc3f004194
ArtFiorindo <ArtFiorindo>
2023-09-28 22:17:21 -0300

    Aula 28-09

--- Ex6.py
+++
@@ -1,7 +0,0 @@
-Cotação = float(input("Digite a cotação do Dólar: "))
-
-Dólar = float(input("Digite o valor da quantidade de dólares: US$ "))
-
-Real = Cotação * Dólar
-
-print(f"O valor US$ {Dólar} em Real pela cotação é R$ {Real}")
--- ÁreaDoTriânguloEx3.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Digite a base do triangulo: "))
-
-b = float(input("Digite a altura do triangulo: "))
-
-r = b * a
-
-s = r / 2
-
-print(f"a área do triangulo é {s: .2f} ")
